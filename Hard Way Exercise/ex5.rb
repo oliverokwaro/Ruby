49b1947c7b1514c51f0c10c838c8bf7c19b4e5b4
oliverokwaro <oliverokwaro@gmail.com>
2016-06-23 16:52:06 +0300

    Ruby head first and hardway tutorial

---
+++ Hard Way Exercise/ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'Oliver Benedict Okwaro'
+my_age = 27 # not a lie
+my_height = 74 # inches
+my_weight = 60 # Kgs only
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'black'
+
+puts "Lets talk about #{my_name}."
+puts "I am #{my_height} inches tall"
+puts "I am #{my_weight} kgs heavy."
+puts "Actually thats not too heavy."
+puts "I have #{my_eyes} eyes and #{my_hair} hair."
+puts "My teeth are actually #{white} depending on the coffee"
+
+# This line is tricky , I will try it to get it exact
+puts "If I add #{my_age}, #{my_height}, and #{weight} I get #{my_age + my_height + my_weight}."
+
