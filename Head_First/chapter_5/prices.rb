886f9b6712733125f5b6a69ec035bfff90222208
oliverokwaro <oliverokwaro@gmail.com>
2016-06-26 22:59:53 +0300

    Arrays

--- Head_First/chapter_5/prices.rb
+++ Head_First/chapter_5/prices.rb
@@ -1,1 +1,33 @@
-puts "Me" 
+def total(prices)
+  amount = 0
+  index = 0
+  while index < prices.length
+    amount += prices[index]
+    index += 1
+  end
+  amount
+end
+
+prices = [3.99, 25.00, 8.99]
+
+puts format("%.2f", total(prices))
+
+def refund(prices)
+  amount = 0
+  index = 0
+  while index < price.length
+    amount -= prices[index]
+    index += 1
+  end
+  amount
+end
+
+def show_discounts(prices)
+  index = 0
+  while index < price.length
+    amount_off = prices[index] / 3.0
+    puts format("Your discount: $%.2f", amount_off)
+    index+= 1
+  end
+end
+
