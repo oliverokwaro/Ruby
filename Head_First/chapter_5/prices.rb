549fbc62d0610066a2c8fa4ea93e0f08757573ca
oliverokwaro <oliverokwaro@gmail.com>
2016-06-27 16:16:50 +0300

    Arrays and Blocks

--- Head_First/chapter_5/prices.rb
+++ Head_First/chapter_5/prices.rb
@@ -1,33 +1,28 @@
 def total(prices)
   amount = 0
-  index = 0
-  while index < prices.length
-    amount += prices[index]
-    index += 1
+  prices.each do |price|
+    amount += price
   end
   amount
 end
 
-prices = [3.99, 25.00, 8.99]
-
-puts format("%.2f", total(prices))
-
 def refund(prices)
   amount = 0
-  index = 0
-  while index < price.length
-    amount -= prices[index]
-    index += 1
+  prices.each do |price|
+    amount -= price
   end
   amount
 end
 
 def show_discounts(prices)
-  index = 0
-  while index < price.length
-    amount_off = prices[index] / 3.0
+ prices.each do |price|
+    amount_off = price / 3.0
     puts format("Your discount: $%.2f", amount_off)
-    index+= 1
   end
 end
+
+prices = [3.99, 25.00, 8.99]
+puts format ("%.2f", refund(peices)) 
+puts format ("%.2f", total(prices))
+show_discounts(prices)
 
