8ddf1d2fc22a77b4a94c349af96bebfb5aefae5b
oliverokwaro <oliverokwaro@gmail.com>
2016-06-30 11:01:17 +0300

    Complete Hash

--- Head_First/chapter_8/reference.rb
+++ Head_First/chapter_8/reference.rb
@@ -1,16 +1,31 @@
 class CelestialBody
 attr_accessor :type, :name
 end
+# Setting default  blocks
+bodies = Hash.new  do |hash, key|
+  body = CelestialBody.new
+  body.type = "planet"
+  hash[key] = body
+  body
+end
 
-altair = CelestialBody.new
-altair.name = 'Altair'
-altair.type = 'Star'
-polaris = CelestialBody.new
-polaris.name ='Polaris'
-polaris.type = 'Star'
-vega = CelestialBody.new
-vega.name = 'Vega'
-vega.type = 'Star'
+bodies['Mars'].name ='Mars'
+bodies['Europa'].name = 'Europa'
+bodies['Europa'].type = 'Moon'
+bodies['Venus'].name = 'Venus'
+
+p  bodies 
+
+
+#altair = CelestialBody.new
+#altair.name = 'Altair'
+#altair.type = 'Star'
+#polaris = CelestialBody.new
+#polaris.name ='Polaris'
+#polaris.type = 'Star'
+#vega = CelestialBody.new
+#vega.name = 'Vega'
+#vega.type = 'Star'
 
 puts altair.name, polaris.name, vega.name
 
