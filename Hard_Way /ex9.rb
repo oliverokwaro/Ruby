2c3ed6a0e5102af0cb9dec4b5c731a7823979341
oliverokwaro <oliverokwaro@gmail.com>
2016-07-06 16:18:34 +0300

    More Printing

---
+++ Hard_Way /ex9.rb
@@ -0,0 +1,15 @@
+# Some new stuff here
+
+days = "Mon Tue Wed Thur Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}
+
+puts """"
+There's something going on here
+with the three double-quotes
+We'll be able to type as much as we like
+Even 4 lines if we want, or 5 or 6
+"""
+
