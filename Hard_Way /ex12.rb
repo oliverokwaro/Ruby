7baa31266f5fe7690c51dbb54986b817f0afe380
oliverokwaro <oliverokwaro@gmail.com>
2016-07-07 10:03:15 +0300

    Gets method.

---
+++ Hard_Way /ex12.rb
@@ -0,0 +1,14 @@
+print "Give me a number"
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "The number is #{bigger}."
+
+print "Give me another number."
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{number}."
+
+
