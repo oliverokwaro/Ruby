9669474e98d23f20cf1781a4c480f90b21dcd7bc
oliverokwaro <oliverokwaro@gmail.com>
2016-07-12 16:54:23 +0300

    Reading files

--- Hard_Way /ex15.rb
+++ Hard_Way /ex15.rb
@@ -1,1 +1,13 @@
+ex15.txt = ARGV.first
 
+txt = open(ex15.txt)
+
+puts "Here's your file #{ex15.txt}:"
+print txt.read
+
+print "Type the filename again: "
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
