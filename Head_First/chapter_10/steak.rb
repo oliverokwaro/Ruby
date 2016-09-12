cbd0964d79d1cec938062b65df88cae5e59c2f8f
oliverokwaro <oliverokwaro@gmail.com>
2016-09-12 20:40:11 +0300

    Enumerables

--- Head_First/chapter_10/steak.rb
+++ Head_First/chapter_10/steak.rb
@@ -1,24 +1,21 @@
 class Steak
-  include Comparable
+  attr_accessor :grade
 
-  GRADE_SC0RES = {"Prime" => 3, "Choice" => 2, "Select" =>1}
+  def >(other)
+    grade_scores = {"Prime" => 3, "Choice" => 2, "Select" => 1}
+    grade_scores[grade] > grade_scores[other.grade]
+  end
 
-attr_accessor :grade
 
-  def <=> (other)
-  if GRADE_SCORES[self.grade] < GRADE_SCORES[other.grade]
-  return -1
-  elsif GRADE_SCORES[self.grade] == GRADE_SCORES[other.grade]
-    return 0
-  else
-    return 1
- end
- end
 end
 
 first_steak = Steak.new
 first_steak.grade = "Prime"
 second_steak = Steak.new
 second_steak.grade = "Choice"
-puts first_steak <=> second_steak
-puts second_steak <=> first_steak
+
+
+if first_steak > second_steak
+  puts "I'll take #{first_steak.inspect} ."
+end
+
