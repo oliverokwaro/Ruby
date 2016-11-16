699a8d11ea741692d3ee95c616d04e60e5989ab2
oliverokwaro <oliverokwaro@gmail.com>
2016-11-16 18:11:00 +0300

    Ex 39

---
+++ Hard_Way /ex39.rb
@@ -0,0 +1,26 @@
+# creating a mapping for state abbreviation 
+
+states = {
+'Oregon' => 'OR',
+'Florida' => 'FL',
+'Carlifonia' => 'CA',
+'New York' => 'NY',
+'Michigan' => 'MI'
+}
+
+#create some basic set of states and cities
+
+cities = {
+  'CA' => 'San Francisco',
+  'MI' => 'Detroit',
+  'FL' => 'Jacksonville'
+}
+
+
+#add some cities
+ cities['NY'] = 'New York'
+ cities['OR'] = 'Portland'
+
+#puts some cities
+ 
+puts '-' * 10
